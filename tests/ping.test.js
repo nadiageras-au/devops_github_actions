64611cc24589da51b2221fe3df6645cf4b609d56
nadia <nadiageras.au@gmail.com>
2025-11-15 12:42:25 +1000

    changed test

--- tests/ping.test.js
+++ tests/ping.test.js
@@ -6,6 +6,6 @@ describe('GET /ping', () => {
     it('should return pong', async () => {
         const res = await request(app).get('/ping');
         expect(res.statusCode).toBe(200);
-        expect(res.body.message).toBe('pong');
+        expect(res.body.message).toBe('ping');
     });
 });
